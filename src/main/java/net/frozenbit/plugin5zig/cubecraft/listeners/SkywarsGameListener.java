9006d98e0a3e683834c8cc95f1a5b086661dc32e
Mark Dresselhaus <m@rk-dresselhaus.de>
2016-11-01 17:05:43 +0100

    Fixed the SkyWars countdown

--- src/main/java/net/frozenbit/plugin5zig/cubecraft/listeners/SkywarsGameListener.java
+++ src/main/java/net/frozenbit/plugin5zig/cubecraft/listeners/SkywarsGameListener.java
@@ -1,6 +1,8 @@
 package net.frozenbit.plugin5zig.cubecraft.listeners;
 
 import eu.the5zig.mod.The5zigAPI;
+import eu.the5zig.mod.server.GameState;
+import eu.the5zig.mod.server.IPatternResult;
 import net.frozenbit.plugin5zig.cubecraft.gamemodes.SkywarsMode;
 
 
@@ -27,4 +29,13 @@ public class SkywarsGameListener extends AbstractCubeCraftGameListener<SkywarsMo
         requestPlayerList();
     }
 
+    @Override
+    public void onMatch(SkywarsMode gameMode, String key, IPatternResult match) {
+        super.onMatch(gameMode, key, match);
+        // temporary workaround until the start message is fixed
+        if (key.equals("skywars.draw.loot")) {
+            gameMode.setState(GameState.GAME);
+            gameMode.setTime(System.currentTimeMillis());
+        }
+    }
 }
