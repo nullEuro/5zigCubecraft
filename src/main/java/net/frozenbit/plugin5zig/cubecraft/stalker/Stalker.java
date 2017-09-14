c03bd28e1f6dc403d57790d4fc161ee6a2f05793
Rasmus Antons <mail@rasmusantons.de>
2017-09-14 02:29:30 +0200

    Partially reverted the last change

--- src/main/java/net/frozenbit/plugin5zig/cubecraft/stalker/Stalker.java
+++ src/main/java/net/frozenbit/plugin5zig/cubecraft/stalker/Stalker.java
@@ -5,9 +5,9 @@ import eu.the5zig.mod.server.GameMode;
 import net.frozenbit.plugin5zig.cubecraft.CubeCraftPlayer;
 import net.frozenbit.plugin5zig.cubecraft.Main;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Stalker {
 
@@ -22,7 +22,7 @@ public class Stalker {
         this.gameMode = gameMode;
         storage = new Storage(gameMode);
         ownName = The5zigAPI.getAPI().getGameProfile().getName();
-        stalkedPlayerList = new ArrayList<>();
+        stalkedPlayerList = new CopyOnWriteArrayList<>();
     }
 
     public void onKill(String victim, String killer) {
